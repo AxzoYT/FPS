6476a62b1e8db54964f519655d8ab39bf807f333
James Shearin <jamesjshearin@gmail.com>
2019-08-24 20:25:57 -0700

    WASD Movement
    Basic Non-Rotational Movement

---
+++ FPSrepo/Assets/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody playerRB;
+
+
+    public KeyCode strafeLeft = KeyCode.A;
+    public KeyCode strafeRight = KeyCode.D;
+    public KeyCode moveForward = KeyCode.W;
+    public KeyCode moveBackward = KeyCode.S;
+
+
+    public float playerMovementSpeed;
+
+
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Debug.Log(Input.mousePosition);
+
+
+
+
+        if (Input.GetKey(strafeLeft))
+        {
+            transform.Translate(-playerMovementSpeed, 0, 0);
+        }
+        if (Input.GetKey(strafeRight))
+        {
+            transform.Translate(playerMovementSpeed, 0, 0);
+        }
+        if (Input.GetKey(moveForward))
+        {
+            transform.Translate(0, 0, playerMovementSpeed);
+        }
+        if (Input.GetKey(moveBackward))
+        {
+            transform.Translate(0, 0, -playerMovementSpeed);
+        }
+    }
+}
