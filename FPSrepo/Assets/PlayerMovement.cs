1f8e73e1effe4c29dd77dd87e655582d19afa775
AxzoYT <kmfxy100@gmail.com>
2019-08-25 06:51:49 -0700

    Update 8/25/19, can now move PM and camera almost fully works. PM still falls through map
    f

---
+++ FPSrepo/Assets/PlayerMovement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody playerRB;
+
+    public KeyCode strafeLeft = KeyCode.A;
+    public KeyCode strafeRight = KeyCode.D;
+    public KeyCode moveForward = KeyCode.W;
+    public KeyCode moveBackward = KeyCode.S;
+    public KeyCode Jump = KeyCode.Space;
+
+
+    public float playerMovementSpeed;
+    public float jumpPower;
+
+
+
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        //Debug.Log(Input.mousePosition);
+
+
+        if (Input.mousePresent == true)
+        {
+        transform.RotateAround(playerRB.position, Vector3.up, Input.GetAxis("Mouse X"));
+        transform.RotateAround(playerRB.position, Vector3.zero, -Input.GetAxis("Mouse Y"));
+        }
+            
+
+
+
+        Debug.Log(Input.GetAxis("Mouse X"));
+
+
+
+
+        if (Input.GetKey(strafeLeft))
+        {
+            transform.Translate(-playerMovementSpeed, 0, 0);
+        }
+        if (Input.GetKey(strafeRight))
+        {
+            transform.Translate(playerMovementSpeed, 0, 0);
+        }
+        if (Input.GetKey(moveForward))
+        {
+            transform.Translate(0, 0, playerMovementSpeed);
+        }
+        if (Input.GetKey(moveBackward))
+        {
+            transform.Translate(0, 0, -playerMovementSpeed);
+        }
+        if (Input.GetKeyDown(Jump))
+        {
+            playerRB.AddRelativeForce(0, jumpPower, 0);
+        }
+
+    }
+
+}
