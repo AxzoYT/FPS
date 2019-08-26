fb579aa41a69a7536cd595c0173cdf3869666ae0
James Shearin <jamesjshearin@gmail.com>
2019-08-26 10:34:20 -0700

    Revert "CameraLock Rotation Fix"
    This reverts commit 365dd665d2cd5b55fa309b06564fc0265ae6ad4b.

--- FPSrepo/Assets/CameraController.cs
+++ FPSrepo/Assets/CameraController.cs
@@ -9,14 +9,6 @@ public class CameraController : MonoBehaviour
     {
         offset = transform.position - player.transform.position;
     }
-
-    void Update()
-    {
-        if (Input.mousePresent == true)
-        {
-            //transform.Rotate(0,transform.position.y + Input.GetAxis("Mouse Y"),0);
-        }
-    }
     void LateUpdate()
     {
         transform.position = player.transform.position + offset;
