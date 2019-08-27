4047e93485b71fe02cda7a4d091dfd1cb75c10d8
AxzoYT <kmfxy100@gmail.com>
2019-08-26 20:56:20 -0700

    wadwad
    wadawd

---
+++ FPSrepo/Assets/CameraController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+    public float maxLookUpAngle = 270;
+    public float maxLookDownAngle = 90;
+
+    
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    void Update()
+    {
+        if (Input.mousePresent)
+        {
+            transform.RotateAround(transform.position, transform.right, -Input.GetAxis("Mouse Y"));
+        }
+        if (transform.localRotation.eulerAngles.z > 0.15f || transform.localRotation.eulerAngles.z < -0.15f)
+        {
+            if (transform.localRotation.eulerAngles.x > maxLookUpAngle)
+            {
+                transform.eulerAngles = new Vector3(maxLookUpAngle, player.transform.eulerAngles.y - 90f, 0f);
+            }
+
+            if (transform.localRotation.eulerAngles.x < maxLookDownAngle)
+            {
+                transform.eulerAngles = new Vector3(maxLookDownAngle, player.transform.eulerAngles.y - 90f, 0f);
+            }
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(transform.localRotation.eulerAngles.x, player.transform.eulerAngles.y - 0f, 0f);
+        }
+    }
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ FPSrepo/Assets/Shooting.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public KeyCode Fire = KeyCode.Mouse0;
+    public GameObject Ammo;
+    public GameObject spawnPoint;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(Fire))
+        {
+            GameObject bullet = Instantiate(Ammo, transform.position + (Vector3.zero * 1), Quaternion.identity);
+            bullet.GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * 10000);
+        }
+
+    }
+}
